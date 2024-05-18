836a046353eeb94899d374576a0de29aa18063d7
ryusuik <newyemac@gmail.com>
2024-05-18 18:36:50 +0900

    5월 18일

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { Routes, Route, Link, useNavigate } from "react-router-dom"
+import Home from "./pages/Home"
+import Diary from "./pages/Diary"
+import New from "./pages/New"
+import Notfound from "./pages/Notfound"
 
+// 이미지 불러오기
+import emotion1 from './assets/emotion1.png'
+import emotion2 from './assets/emotion2.png'
+import emotion3 from './assets/emotion3.png'
+import emotion4 from './assets/emotion4.png'
+import emotion5 from './assets/emotion5.png'
+
+
+// 1. "/" : 모든 일기를 조회하는 HOme 페이지
+//2. "/new" : 새로운 일기를 작성하는 New 페이지
+//3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const [count, setCount] = useState(0)
+  const nav = useNavigate();
+  const onclickButton = () => {
+    nav("/new")
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <img src={emotion1} />
+      <img src={emotion2} />
+      <img src={emotion3} />
+      <img src={emotion4} />
+      <img src={emotion5} />
+    </div>
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/new"}>New</Link>
+      <Link to={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onclickButton}>New페이지로 이동</button>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary/:id" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  </>
+  )  
 }
 
-export default App
+export default App;
